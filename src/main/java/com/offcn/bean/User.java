cc8b6be2336f998b8ac580ceeb16df70505a7e2a
lidahai <435421324@qq.com>
2020-09-23 21:27:19 +0800

    ffer

--- src/main/java/com/offcn/bean/User.java
+++ src/main/java/com/offcn/bean/User.java
@@ -9,6 +9,10 @@ public class User {
     public User() {
     }
 
+    public User(Integer id) {
+        this.id = id;
+    }
+
     public User(Integer id, String name, int age) {
         this.id = id;
         this.name = name;
